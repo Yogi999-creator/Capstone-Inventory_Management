b53177edd97cdc1af3d75b535eb2a0a3d1e8d976
Yogi999-creator <yogesh.mundhra18@st.niituniversity.in>
2021-04-24 22:23:15 +0530

    Class Based Inventory

---
+++ Store/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import ItemsRegistration
+from .models import User
+from django.views.generic.base import TemplateView, RedirectView
+from django.views import View
+# Create your views here.
+
+# This Class Will Add new Item and Show All Items.
+class UserAddShowView(TemplateView):
+    template_name = 'Store/addandshow.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        fm = ItemsRegistration()
+        item = User.objects.all()
+        context = {'ite':item, 'form':fm}
+        return context
+    def post(self, request):
+         fm = ItemsRegistration(request.POST)
+         if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+         return HttpResponseRedirect('/')
+
+# This Class Will Update/Edit.
+class UserUpdateView(View):
+    def get(self, request, id):
+         pi = User.objects.get(pk=id)
+         fm = ItemsRegistration(instance=pi)
+         return render(request,'Store/update.html', {'form':fm})
+    
+    def post(self, request, id):
+        pi = User.objects.get(pk=id)
+        fm = ItemsRegistration(request.POST, instance=pi)
+        if fm.is_valid():
+           fm.save()
+        return HttpResponseRedirect('/')
+
+# This Class will delete.
+class UserDeleteView(RedirectView):
+    url = '/'
+    def get_redirect_url(self, *args, **kwargs):
+        del_id = kwargs['id']
+        User.objects.get(pk=del_id).delete()
+        return super().get_redirect_url(*args, **kwargs)
